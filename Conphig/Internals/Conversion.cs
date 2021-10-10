543bf78297d4bbdd9c05da17ddc316051826a4bc
Anders Marzi Tornblad <anders.tornblad@gmail.com>
2021-10-10 16:13:09 +0200

    Refactored another large method

--- Conphig/Internals/Conversion.cs
+++ Conphig/Internals/Conversion.cs
@@ -39,49 +39,7 @@ namespace ATornblad.Conphig.Internals
 
             if (input is string str)
             {
-                if (str.Length == 0 && targetType.IsClass)
-                {
-                    return null;
-                }
-                else if (targetType == typeof(DateTime))
-                {
-                    return DateTime.Parse(str, CultureInfo.InvariantCulture, DateTimeStyles.AdjustToUniversal);
-                }
-                else if (targetType == typeof(DateTimeOffset))
-                {
-                    return DateTimeOffset.Parse(str, CultureInfo.InvariantCulture, DateTimeStyles.AssumeUniversal);
-                }
-                else if (targetType == typeof(bool))
-                {
-                    return str.ToLower() == "true";
-                }
-                else if (targetType == typeof(int))
-                {
-                    return int.Parse(str, NumberStyles.Integer, CultureInfo.InvariantCulture);
-                }
-                else if (targetType == typeof(long))
-                {
-                    return long.Parse(str, NumberStyles.Integer, CultureInfo.InvariantCulture);
-                }
-                else if (targetType == typeof(float))
-                {
-                    return float.Parse(str, NumberStyles.Float, CultureInfo.InvariantCulture);
-                }
-                else if (targetType == typeof(double))
-                {
-                    return double.Parse(str, NumberStyles.Float, CultureInfo.InvariantCulture);
-                }
-                else
-                {
-                    try
-                    {
-                        return Convert.ChangeType(str, targetType);
-                    }
-                    catch
-                    {
-                        throw new InvalidCastException($"Cannot convert a string value to {targetType.FullName}");
-                    }
-                }
+                return ConvertString(str, targetType);
             }
 
             try
@@ -91,6 +49,53 @@ namespace ATornblad.Conphig.Internals
             catch
             {
                 throw new InvalidCastException($"Cannot convert a {input.GetType().FullName} value to {targetType.FullName}");
+            }
+        }
+
+        private static object ConvertString(string str, Type targetType)
+        {
+            if (str.Length == 0 && targetType.IsClass)
+            {
+                return null;
+            }
+            else if (targetType == typeof(DateTime))
+            {
+                return DateTime.Parse(str, CultureInfo.InvariantCulture, DateTimeStyles.AdjustToUniversal);
+            }
+            else if (targetType == typeof(DateTimeOffset))
+            {
+                return DateTimeOffset.Parse(str, CultureInfo.InvariantCulture, DateTimeStyles.AssumeUniversal);
+            }
+            else if (targetType == typeof(bool))
+            {
+                return str.ToLower() == "true";
+            }
+            else if (targetType == typeof(int))
+            {
+                return int.Parse(str, NumberStyles.Integer, CultureInfo.InvariantCulture);
+            }
+            else if (targetType == typeof(long))
+            {
+                return long.Parse(str, NumberStyles.Integer, CultureInfo.InvariantCulture);
+            }
+            else if (targetType == typeof(float))
+            {
+                return float.Parse(str, NumberStyles.Float, CultureInfo.InvariantCulture);
+            }
+            else if (targetType == typeof(double))
+            {
+                return double.Parse(str, NumberStyles.Float, CultureInfo.InvariantCulture);
+            }
+            else
+            {
+                try
+                {
+                    return Convert.ChangeType(str, targetType);
+                }
+                catch
+                {
+                    throw new InvalidCastException($"Cannot convert a string value to {targetType.FullName}");
+                }
             }
         }
     }
